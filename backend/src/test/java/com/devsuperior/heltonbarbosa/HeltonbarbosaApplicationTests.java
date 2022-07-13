7c1ae125aef355fad9594ee31021f0fc5ea885b8
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2022-07-13 10:04:06 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/heltonbarbosa/HeltonbarbosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.heltonbarbosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeltonbarbosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
